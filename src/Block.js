75ecec894ac2fee6da478987e8221641e31ef9c7
Gabriel Vasconcelos <138534514+devitruvius@users.noreply.github.com>
2024-10-10 12:20:09 -0300

    refactor: move cálculo de hash para função utilitária

--- src/Block.js
+++ src/Block.js
@@ -1,24 +1,37 @@
+// Importações.
+const crypto = require("crypto");
+
+// Função utilitária para calcular o hash.
+function calculateBlockHash(index, previousHash, timestamp, transactions, nonce) {
+    const stringifiedDAta = JSON.stringify({ index, previousHash, timestamp, transactions, nonce });
+    return crypto
+        .createHash("sha256")
+        .update(stringifiedData)
+        .digest("hex");
+}
+
 // Classe Block.
 class Block {
-    constructor(index, previousHash, timestamp, data, hash, nonce) {
+    constructor(index, previousHash, timestamp, transactions, hash, nonce) {
         this.index = index;
         this.previousHash = previousHash;
         this.timestamp = timestamp;
-        this.data = data;
+        this.transactions = transactions;
         this.hash = hash;
         this.nonce = nonce;
     }
 
     // Método estático de retorno do bloco gênesis.
-    static get genesis(){
-        return new Block(
-            0,
-            "0",
-            1678886400000,
-            "Bloco Gênesis",
-            "81653231e7016c82073a616e1715c4b77376371202504af54f58073ed6507b51",
-            0
-        );
+    static get genesis() {
+        const genesisBlock = new Block(0, "0", 1678886400000, [], null, 0);
+        genesisBlock.hash = calculateBlockHash(
+            genesisBlock.index,
+            genesisBlock.previousHash,
+            genesisBlock.timestamp,
+            genesisBlock.transactions,
+            genesisBlock.nonce
+        )
+        return genesisBlock;
     }
 }
 
